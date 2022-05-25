fa9446b8045db4adc6a60412ecd641407412667b
Johnpaul Eze <ezejohnpaul846@gmail.com>
2022-05-25 15:43:58 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Dropzone from './Dropzone';
+
+function App() {
+  return (
+    <div>
+      <Dropzone />
+    </div>
+  );
+}
+
+
+export default App;
---
+++ src/Dropzone.js
@@ -0,0 +1,89 @@
+import React, { useState, useCallback, useEffect } from 'react';
+import { useDropzone } from "react-dropzone";
+import "./App.css";
+
+function Dropzone() {
+  const [files, setFiles] = useState([]);
+
+  const onDrop = useCallback((acceptedFiles) => {
+    console.log(acceptedFiles);
+    setFiles(
+      acceptedFiles.map((file) =>
+        Object.assign(file, {
+          preview: URL.createObjectURL(file)
+        })
+      )
+    );
+  }, []);
+
+  const {
+    getRootProps,
+    getInputProps,
+    isDragActive,
+  } = useDropzone({
+    onDrop,
+    accept: 'images/*',
+  });
+
+  const thumbs = files.map(file => (
+    <div key={file.name}>
+      <div >
+        <img
+          src={file.preview}
+          onLoad={() => { URL.revokeObjectURL(file.preview) }} alt=""
+        />
+      </div>
+    </div>
+  ));
+  
+  useEffect(() => {
+  return () => files.forEach(file => URL.revokeObjectURL(file.preview));
+  },[files]);
+ 
+  return (
+    <div className="App">
+      <div>
+      <div
+    {...getRootProps()}>
+          <input {...getInputProps()} />
+          {isDragActive ? (
+            <p className="dropzone">Drop your image files here</p>
+          ) : (
+            <p className="dropzone">
+              Drag and drop some files here, or click to select files
+            </p>
+          )}
+        </div>
+      </div>
+      {files.length > 0 && (
+        <button className='button'
+          onClick={() => {
+            const url = "https://api.cloudinary.com/v1_1/johnpaul/image/upload";
+            const formData = new FormData();
+            let file = files[0];
+            formData.append("file", file);
+            formData.append("upload_preset", "dryqolej");
+            fetch(url, {
+              method: "POST",
+              body: formData
+            })
+              .then((response) => {
+                return response.json();
+              })
+              .then((data) => {
+                console.log(data);
+              });
+          }}
+        >
+          Upload
+        </button>
+      )}
+
+      <aside>
+        {thumbs}
+      </aside>
+    </div>
+  );
+}
+
+export default Dropzone;
